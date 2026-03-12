06fa0f34dfaa2403bf49bc8e58af85cf384fa0b2
danibrb <daniele.mkr@gmail.com>
2026-03-12 14:48:38 +0100

    feat: add basic structure of code

---
+++ reticular_gas/config.py
@@ -0,0 +1,10 @@
+"""
+Simulation configuration
+"""
+
+L: int = 8           # Matrix size
+theta: float = .5      # Filling
+
+# Reproducibility
+
+seed = 1234567890
---
+++ reticular_gas/visualization.py
@@ -0,0 +1,8 @@
+"""
+Visualization of the grating
+"""
+
+import numpy as np
+import matplotlib as plt
+
+
